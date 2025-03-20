83644320eb70a1efad1ee0804c80b67e42ded618
Harsha-S2604 <harsha91148@outlook.com>
2025-03-20 23:23:50 +0530

    useContract hook

---
+++ src/hooks/useContract.jsx
@@ -0,0 +1,21 @@
+const useContract = () => {
+    const addContract = async () => {
+
+    }
+
+    const deleteContract = async () => {
+
+    }
+
+    const updateContract = async () => {
+
+    }
+
+    return {
+        addContract,
+        deleteContract,
+        updateContract
+    }
+}
+
+export default useContract
