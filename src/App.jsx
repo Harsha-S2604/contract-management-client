2f6049d003cbc2f722915946f6a61ee77090d6e5
Harsha-S2604 <harsha91148@outlook.com>
2025-03-20 13:16:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-14 items-center justify-center">
+          <div className="text-center">
+            <h2 className="text-3xl dark:text-white font-bold">Contract Management</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
