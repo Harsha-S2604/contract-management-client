5812827f6db2bdb4354106c0da223ad144b2683b
Harsha-S2604 <harsha91148@outlook.com>
2025-03-20 13:30:52 +0530

    basic setup - import resolver '@' - Heading Component -- displays the heading - Contracts Component -- without implementation of displaying contract details

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
+import Heading from './components/Heading'
+import Contracts from './components/Contracts'
 
 function App() {
 
   return (
-    <div className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-14 items-center justify-center">
-          <div className="text-center">
-            <h2 className="text-3xl dark:text-white font-bold">Contract Management</h2>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Heading title="Contract Management" />
+      <Contracts />
     </div>
   )
 }
---
+++ src/components/Heading/index.jsx
@@ -0,0 +1,16 @@
+const Heading = (props) => {
+    const heading = props?.title || 'Contract Management'
+    return (
+        <div className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="flex h-14 items-center justify-center">
+                    <div className="text-center">
+                        <h2 className="text-3xl dark:text-white font-bold">{heading}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Heading
